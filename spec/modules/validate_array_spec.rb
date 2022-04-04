393ebeee767916a7637cdc587c2fbd3163cabedd
NiyantaPrasad <niyanta@zwayam.com>
2022-04-04 18:03:00 +0530

    FactoryBot defined for valid and invalid array

--- spec/modules/validate_array_spec.rb
+++ spec/modules/validate_array_spec.rb
@@ -5,15 +5,27 @@ require_relative '../../lib/validate_array'
 
 RSpec.describe ValidateArray do
 
-  let(:valid_array) { [1, 0, 2, -3, 1, 2, 1] }
-  
-  let(:invalid_array) { [1, 0, "a", -1, 0.1] }
+  FactoryBot.define do
+    factory :sub_array do
+      trait :valid_array do
+        integer_array { [1, 0, 2, -3, 1, 2, 1] }
+      end
+
+      trait :invalid_array do
+        integer_array { [1, 0, "a", -1, 0.1] }
+      end
+    end
+  end
+
+  let(:max_array_sum) { MaxArraySumsController.new }
 
   it "should return an array" do
-    expect(ValidateArray.check_for_valid_array(array: valid_array)).to be_an(Array)
+  	valid_array   = FactoryBot.create :sub_array, :valid_array
+    expect(max_array_sum.check_for_valid_array(array: valid_array.integer_array)).to be_an(Array)
   end
 
    it "should not return an array" do
-    expect(ValidateArray.check_for_valid_array(array: invalid_array)).not_to be_an(Array)
+  	invalid_array = FactoryBot.create :sub_array, :invalid_array
+    expect(max_array_sum.check_for_valid_array(array: invalid_array.integer_array)).not_to be_an(Array)
   end
 end
